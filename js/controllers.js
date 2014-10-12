bf0b82db424deb1df25c57f390ae284843330180
antonradev <anton.radev@gmail.com>
2014-10-12 22:33:46 +0300

    Basic controller added to connect to a piece of the DB and print it to the UI. Added some SASS mixins and styles and Form for status posting.

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,15 @@
+NGGab.controller("DefaultInfoCtrl", function ($scope, $http) {
+    $http.get('rest/index.php/api/app_info/info/format/json').
+            success(function (data, status, headers, config) {
+                $scope.dafaultInfoArray = data;
+            }).
+            error(function (data, status, headers, config) {
+                // log error
+            });
+});
+
+
+
 NGGab.controller('DefaultData', ['$scope', function ($scope) {
         $scope.appDefaults = {
             appName: 'ng-gab',
